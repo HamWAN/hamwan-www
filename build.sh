0e4fa421925b6a380062a7999ce72d47e631b170
Bart Kus <me@bartk.us>
2016-07-31 20:25:32 -0700

    Quick and dirty menus

--- build.sh
+++ build.sh
@@ -9,12 +9,27 @@ else
 fi
 rm -rf $TMP_DIR
 mkdir $TMP_DIR
+
+# Pass 1: Generate the menu.html contents
 find src -type f -name "*.md" -print0 | while read -d $'\0' FILE
 do
 	HTML_FILE=`echo "$FILE" | sed -e 's/\.md$/.html/' -e 's/^src\///'` # Replace extension .md -> .html
-	pandoc -s "$FILE" -o "$TMP_DIR/$HTML_FILE"
+	echo "<a href=\"/$HTML_FILE\">$HTML_FILE</a>" >> $TMP_DIR/menu.html
 done
+
+# Pass 2: Generate the remaining web contents, incorporating a full menu.html now
+find src -type f -name "*.md" -print0 | while read -d $'\0' FILE
+do
+	HTML_FILE=`echo "$FILE" | sed -e 's/\.md$/.html/' -e 's/^src\///'` # Replace extension .md -> .html
+	pandoc -s -B $TMP_DIR/menu.html "$FILE" -o "$TMP_DIR/$HTML_FILE"
+done
+
+# Now publish any resources that might be left in the src tree (images, javascript, etc)
 rsync -a src/ $TMP_DIR/
+
+# And do a not-quite-atomic-but-pretty-fast switchover of the live website to the new version
 rm out
 ln -s $TMP_DIR out
+
+# And return the user to their normal working directory
 popd >/dev/null
---
+++ src/second.md
@@ -0,0 +1,4 @@
+This is a Second Page
+=====================
+
+Used for testing the new menu feature.
